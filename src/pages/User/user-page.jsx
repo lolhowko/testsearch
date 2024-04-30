29c3bf8e20d026159d04e50aea5a4d237f64d31c
Lana <lolhowko@gmail.com>
2024-05-01 01:37:35 +0300

    add loader to user-page

--- src/pages/User/user-page.jsx
+++ src/pages/User/user-page.jsx
@@ -18,75 +18,79 @@ export const UserPage = () => {
   return (
     <S.Wrapper>
       {error === undefined ? (
-        <>
-          <S.UserPageTittle>
-            Информация о <S.UserPageTittleName>{userId}</S.UserPageTittleName>
-          </S.UserPageTittle>
-          <S.UserAvatar src={currentUserData?.avatar_url} alt="avatar" />
+        isLoading ? (
+          <S.Message>Подождите, идёт загрузка..</S.Message>
+        ) : (
+          <>
+            <S.UserPageTittle>
+              Информация о <S.UserPageTittleName>{userId}</S.UserPageTittleName>
+            </S.UserPageTittle>
+            <S.UserAvatar src={currentUserData?.avatar_url} alt="avatar" />
 
-          <S.UserContent>
-            <S.UserDataText>
-              Cсылка на аккаунт:
-              <S.UserDataUrl href={currentUserData?.html_url}>
-                {currentUserData?.html_url === null ? (
-                  <S.UserDataTextSpan>не найденo</S.UserDataTextSpan>
-                ) : (
-                  currentUserData?.html_url
-                )}
-              </S.UserDataUrl>
-            </S.UserDataText>
-            <S.UserDataText>
-              Имя пользователя:
-              <S.UserDataTextSpan>
-                {currentUserData?.name === null ? (
-                  <S.UserDataTextSpan>не найденo</S.UserDataTextSpan>
-                ) : (
-                  currentUserData?.name
-                )}
-              </S.UserDataTextSpan>
-            </S.UserDataText>
-            <S.UserDataText>
-              email:
-              <S.UserDataTextSpan>
-                {currentUserData?.email === null ? (
-                  <S.UserDataTextSpan>не найден</S.UserDataTextSpan>
-                ) : (
-                  currentUserData?.email
-                )}
-              </S.UserDataTextSpan>
-            </S.UserDataText>
-            <S.UserDataText>
-              Количество подписчиков:
-              <S.UserDataTextSpan>
-                {currentUserData?.followers === null ? (
-                  <S.UserDataTextSpan>не найденo</S.UserDataTextSpan>
-                ) : (
-                  currentUserData?.followers
-                )}
-              </S.UserDataTextSpan>
-            </S.UserDataText>
-            <S.UserDataText>
-              Количество подписок:
-              <S.UserDataTextSpan>
-                {currentUserData?.following === null ? (
-                  <S.UserDataTextSpan>не найденo</S.UserDataTextSpan>
-                ) : (
-                  currentUserData?.following
-                )}
-              </S.UserDataTextSpan>
-            </S.UserDataText>
-            <S.UserDataText>
-              Количество репозиториев:
-              <S.UserDataTextSpan>
-                {currentUserData?.public_repos === null ? (
-                  <S.UserDataTextSpan>не найденo</S.UserDataTextSpan>
-                ) : (
-                  currentUserData?.public_repos
-                )}
-              </S.UserDataTextSpan>
-            </S.UserDataText>
-          </S.UserContent>
-        </>
+            <S.UserContent>
+              <S.UserDataText>
+                Cсылка на аккаунт:
+                <S.UserDataUrl href={currentUserData?.html_url}>
+                  {currentUserData?.html_url === null ? (
+                    <S.UserDataTextSpan>не найденo</S.UserDataTextSpan>
+                  ) : (
+                    currentUserData?.html_url
+                  )}
+                </S.UserDataUrl>
+              </S.UserDataText>
+              <S.UserDataText>
+                Имя пользователя:
+                <S.UserDataTextSpan>
+                  {currentUserData?.name === null ? (
+                    <S.UserDataTextSpan>не найденo</S.UserDataTextSpan>
+                  ) : (
+                    currentUserData?.name
+                  )}
+                </S.UserDataTextSpan>
+              </S.UserDataText>
+              <S.UserDataText>
+                email:
+                <S.UserDataTextSpan>
+                  {currentUserData?.email === null ? (
+                    <S.UserDataTextSpan>не найден</S.UserDataTextSpan>
+                  ) : (
+                    currentUserData?.email
+                  )}
+                </S.UserDataTextSpan>
+              </S.UserDataText>
+              <S.UserDataText>
+                Количество подписчиков:
+                <S.UserDataTextSpan>
+                  {currentUserData?.followers === null ? (
+                    <S.UserDataTextSpan>не найденo</S.UserDataTextSpan>
+                  ) : (
+                    currentUserData?.followers
+                  )}
+                </S.UserDataTextSpan>
+              </S.UserDataText>
+              <S.UserDataText>
+                Количество подписок:
+                <S.UserDataTextSpan>
+                  {currentUserData?.following === null ? (
+                    <S.UserDataTextSpan>не найденo</S.UserDataTextSpan>
+                  ) : (
+                    currentUserData?.following
+                  )}
+                </S.UserDataTextSpan>
+              </S.UserDataText>
+              <S.UserDataText>
+                Количество репозиториев:
+                <S.UserDataTextSpan>
+                  {currentUserData?.public_repos === null ? (
+                    <S.UserDataTextSpan>не найденo</S.UserDataTextSpan>
+                  ) : (
+                    currentUserData?.public_repos
+                  )}
+                </S.UserDataTextSpan>
+              </S.UserDataText>
+            </S.UserContent>
+          </>
+        )
       ) : (
         <S.Message> Пользователь не найден </S.Message>
       )}
