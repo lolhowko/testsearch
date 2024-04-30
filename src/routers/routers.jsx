35e927dd45d02da5cb743a5f9b72e4d6075a74b1
Lana <lolhowko@gmail.com>
2024-04-30 20:58:33 +0300

    add user-page

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,5 +1,6 @@
 import { MainPage } from "../pages/Main";
 import { NotFound } from "../pages/NotFound";
+import { UserPage } from "../pages/User/user-page";
 
 const { Routes, Route } = require("react-router-dom");
 
@@ -7,6 +8,7 @@ export const AppRouters = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/userPage/:userId" element={<UserPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
